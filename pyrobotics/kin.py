a37a7fa8702ee2d5cfc60f3e25e0f44a0d278cbc
berwul <bernhard.f.k.wullt@gmail.com>
2022-02-15 10:18:09 +0100

    Update with kin

---
+++ pyrobotics/kin.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+
+def SE3_mul(T, p):
+    return T[0:3, 0:3] @ p + T[0:3, 3]
+
+
+def rot_trans_to_SE3(R=None, p=None):
+    T = np.eye(4)
+    if R is not None:
+        T[0:3, 0:3] = R
+    if p is not None:
+        p = p.ravel()
+        T[0: p.shape[0], 3] = p
+    return T
+
+
+def rot_z_to_SO3(angle):
+    R = np.eye(3)
+    R[0, 0] = np.cos(angle)
+    R[1, 1] = np.cos(angle)
+    R[0, 1] = -np.sin(angle)
+    R[1, 0] = np.sin(angle)
+    return R
+
+
+# def angle_from_SE3(T):
+#     return np.arctan2(T[1, 0], T[0, 0])
+
+
+def vec_to_skew(w_vec):
+    w_vec = w_vec.reshape(-1)
+    w_skew = np.zeros((3, 3))
+    w_skew[0, 1] = -w_vec[2]
+    w_skew[1, 0] = w_vec[2]
+    w_skew[0, 2] = w_vec[1]
+    w_skew[2, 0] = -w_vec[1]
+    w_skew[1, 2] = -w_vec[0]
+    w_skew[2, 1] = w_vec[0]
+    return w_skew
+
+
+def g_theta(w_skew, theta):
+    return np.eye(3) * theta + (1 - np.cos(theta)) * w_skew + (theta - np.sin(theta)) * w_skew.dot(w_skew)
+
+
+def SO3_exp(w_skew, theta):
+    return np.eye(3) + w_skew * np.sin(theta) + (1 - np.cos(theta)) * w_skew.dot(w_skew)
+
+
+def SE3_exp(screw_vec, theta):
+    w, v = screw_vec[0:3], screw_vec[3::]
+    w_skew = vec_to_skew(w)
+    g = g_theta(w_skew, theta)
+    p = g.dot(v)
+    R = SO3_exp(w_skew, theta)
+    T = np.eye(4)
+    T[0:3, 0:3] = R
+    T[0:3, 3] = p
+    return T
+
+
+class KinematicChain:
+
+    def __init__(self, kinematics):
+        self.nr_joints = len(kinematics) - 1
+        self.config = np.zeros((self.nr_joints,))
+        self.frames = np.c_[[np.eye(4) for _ in range(self.nr_joints + 1)]]
+        self.screws = np.zeros((self.nr_joints, 6))
+        self.initialize_parts(kinematics)
+
+    def initialize_parts(self, kinematics):
+        kin_vec_total = np.zeros((3,))
+        for i, (pos_rel_parent, rot_vec) in enumerate(kinematics[:-1]):
+            kin_vec_total += pos_rel_parent
+            self.frames[i, 0:3, 3] = kin_vec_total
+            self.screws[i, 0:3] = rot_vec
+            self.screws[i, 3:] = np.cross(kin_vec_total, rot_vec)
+        pos_ee, *_ = kinematics[-1]
+        self.frames[-1, :3, 3] = kin_vec_total + pos_ee
+
+    def set_config(self, values):
+        self.config = values
+        self.transforms = self.forward(values)
+
+    def forward(self, angles, include_ee=False):
+        nr_frames = self.nr_joints + 1 if include_ee else self.nr_joints
+        kinematic_chain = [
+            (self.screws[i, :], angles[i], self.frames[i, :, :])
+            if i != self.nr_joints else
+            (None, None, self.frames[i, :, :])
+            for i in range(nr_frames)
+        ]
+        T = np.eye(4)
+        all_cs = []
+        for screw_vec, theta, cs in kinematic_chain:
+            if screw_vec is not None:
+                T = T @ SE3_exp(screw_vec, theta)
+            all_cs.append(T @ cs)
+        return all_cs
+
+
+if __name__ == "__main__":
+    kinematics = [
+        (np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0])),
+        (np.array([1.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0])),
+        (np.array([1.0, 0.0, 0.0]), None)
+    ]
+    chain = KinematicChain(kinematics)
+    ts = chain.forward(np.array([-np.pi/2, 0]), include_ee=True)
+
+    for t in ts:
+        print("")
+        print(t)
+
+    # kinematics = [
+    #     (np.array([0.0, 0.1, 0.0]), np.array([0.0, 1.0, 0.0])),
+    #     (np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0])),
+    #     (np.array([0.3, 0.0, 0.0]), np.array([0.0, 0.0, 1.0])),
+    #     (np.array([0.3, 0.0, 0.0]), None)
+    # ]
+    # link_geometries = [
+    #     (np.array([0.1, 0.1, 0.1]), np.array([0.0, 0.1, 0.0])),
+    #     (np.array([0.3, 0.1, 0.1]), np.array([0.3, 0.0, 0.0])),
+    #     (np.array([0.3, 0.1, 0.1]), np.array([0.3, 0.0, 0.0])),
+    # ]
+    # chain = KinematicChain(kinematics)
+    # ts = chain.forward(np.array([np.pi/2, 0.0, 0.0]))
+    # for t in ts:
+    #     print(t)
---
+++ pyrobotics/utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def generate_random_trajectory(joint_limits, nr_via_points=10, delta_t=0.1, nr_points=500):
+    nr_joints = joint_limits.shape[0]
+    beta = np.hstack([np.random.uniform(l, u, (nr_via_points,)).reshape(-1, 1) for l, u in joint_limits])
+    beta_d = np.random.uniform(0, 2 * np.pi, (nr_via_points, nr_joints))
+    coeffs = []
+    for i in range(nr_via_points):
+        i_nxt = (i + 1) % nr_via_points
+        a_0 = beta[i]
+        a_1 = beta_d[i]
+        a_2 = 3 * beta[i_nxt] - 3 * beta[i] - 2 * beta_d[i] * delta_t - beta_d[i_nxt] * delta_t
+        a_2 /= (delta_t ** 2)
+        a_3 = 2 * beta[i] + (beta_d[i] + beta_d[i_nxt]) * delta_t - 2 * beta[i_nxt]
+        a_3 /= (delta_t ** 3)
+        coeffs.append((a_0, a_1, a_2, a_3))
+    return calculate_poses(coeffs, nr_via_points, delta_t, nr_points=nr_points)
+
+
+def calculate_poses(coeffs, nr_viapoints, delta_t, nr_points=500):
+    N = nr_viapoints
+    ts = np.linspace(0, N * delta_t, nr_points)
+    poses = []
+    for t in ts:
+        t %= (delta_t * N)
+        t_trunc = int(t // delta_t)
+        a_0, a_1, a_2, a_3 = coeffs[t_trunc]
+        dt = t - (t_trunc * delta_t)
+        pos = a_0 + a_1 * dt + a_2 * dt ** 2 + a_3 * dt ** 3
+        poses.append(pos)
+    return np.c_[poses]
