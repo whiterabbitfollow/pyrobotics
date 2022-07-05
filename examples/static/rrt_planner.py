2c6c9d1bce33706c854b9cec101f0a5dff2ebcb4
berwul <bernhard.f.k.wullt@gmail.com>
2022-07-05 14:22:45 +0200

    Created static examples

--- examples/static/rrt_planner.py
+++ examples/static/rrt_planner.py
@@ -4,22 +4,19 @@ import matplotlib
 
 import numpy as np
 
-from examples.static_world import StaticBoxesWorld
-from pyrb.mp.planners.rrt_star import RRTStarPlanner, RRTStarPlannerModified, logger
+from examples.static.static_world import StaticBoxesWorld
+from pyrb.mp.planners.rrt import RRTPlannerModified
 
-import logging
-
-logger.setLevel(logging.DEBUG)
 
 matplotlib.rc("font", size=16)
 
 world = StaticBoxesWorld()
 world.reset()
 
-planner = RRTStarPlannerModified(world, max_nr_vertices=int(1e4), nearest_radius=0.4)
+planner = RRTPlannerModified(world, max_nr_vertices=int(1e4))
+
 q_start = planner.sample_collision_free_config()
 q_goal = planner.sample_collision_free_config()
-
 path, status = planner.plan(q_start, q_goal, max_planning_time=20)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
@@ -27,24 +24,22 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
 world.render_world(ax1)
 world.render_configuration_space(ax2)
 
+vert_cnt = planner.vert_cnt
+verts = planner.vertices
+ax2.scatter(verts[:vert_cnt, 0], verts[:vert_cnt, 1], color="black")
+
 for i_parent, indxs_children in planner.edges_parent_to_children.items():
     for i_child in indxs_children:
         q = np.stack([planner.vertices[i_parent], planner.vertices[i_child]], axis=0)
         ax2.plot(q[:, 0], q[:, 1], color="black")
 
-
 ax2.scatter(q_start[0], q_start[1], color="green", label="start, $q_I$")
 ax2.scatter(q_goal[0], q_goal[1], color="red", label="goal, $q_G$")
-
-vert_cnt = planner.vert_cnt
-verts = planner.vertices
-ax2.scatter(verts[:vert_cnt, 0], verts[:vert_cnt, 1], color="black")
 
 if path:
     path = np.array(path)
     ax2.plot(path[:, 0], path[:, 1], color="blue", label="path")
     ax2.scatter(path[:, 0], path[:, 1], color="blue")
-
 
 ax2.add_patch(Circle(q_goal, radius=planner.goal_region_radius, alpha=0.2, color="red"))
 ax2.add_patch(Circle(q_start, radius=0.04, color="green"))
