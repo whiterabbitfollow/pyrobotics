ceac851a51dd2cb939b3c30bf8ccb8f1fe6f2c9f
berwul <bernhard.f.k.wullt@gmail.com>
2022-08-10 17:04:05 +0200

    Added data scripts

--- pyrb/mp/planners/moving/rrt.py
+++ pyrb/mp/planners/moving/rrt.py
@@ -1,13 +1,14 @@
+from collections import defaultdict
+import logging
 import math
 import time
 
 import numpy as np
-from collections import defaultdict
-
 
 from pyrb.mp.base_world import BaseMPTimeVaryingWorld
 from pyrb.mp.planners.shared import PlanningData, Status
 
+logger = logging.Logger(__name__)
 
 def is_vertex_in_goal_region(q, q_goal, goal_region_radius):
     distance = np.linalg.norm(q - q_goal)
@@ -85,13 +86,11 @@ class RRTPlannerTimeVarying:
     def __init__(
             self,
             world: BaseMPTimeVaryingWorld,
-            time_horizon,
             max_nr_vertices=int(1e4),
             local_planner_max_distance=0.5,
             local_planner_nr_coll_steps=10,
             goal_region_radius=1e-1
     ):
-        self.time_horizon = time_horizon
         self.goal_region_radius = goal_region_radius
         self.max_nr_vertices = max_nr_vertices
         self.max_actuation = world.robot.max_actuation
@@ -117,7 +116,14 @@ class RRTPlannerTimeVarying:
         self.edges_parent_to_children.clear()
         self.vert_cnt = 0
 
-    def plan(self, state_start, config_goal, max_planning_time=np.inf, min_planning_time=0):
+    def plan(
+            self,
+            state_start,
+            config_goal,
+            max_planning_time=np.inf,
+            min_planning_time=0,
+            time_horizon=300
+    ):
         self.add_vertex_to_tree(state_start)
         time_s, time_elapsed = self.start_timer()
         found_solution = False
@@ -126,7 +132,7 @@ class RRTPlannerTimeVarying:
                 time_elapsed < max_planning_time and
                 not found_solution
         ) or time_elapsed < min_planning_time:
-            state_free = self.sample_collision_free_config()
+            state_free = self.sample_collision_free_config(time_horizon)
             i_nearest, state_nearest = self.find_nearest_vertex(state_free)
             if i_nearest is None:
                 continue
@@ -139,10 +145,10 @@ class RRTPlannerTimeVarying:
                 if is_vertex_in_goal_region(config_new, config_goal, self.goal_region_radius):
                     found_solution = True
                     if time_elapsed < min_planning_time:
-                        self.time_horizon = state_new[-1]
+                        time_horizon = state_new[-1]
                         print(
-                            f"Found solution with time horizon {self.time_horizon}."
-                            f" Min planning time left {min_planning_time}"
+                            f"Found solution with time horizon {time_horizon}."
+                            f" Min planning time left {min_planning_time - time_elapsed}"
                         )
                     break
                 i_parent = i_child
@@ -162,7 +168,6 @@ class RRTPlannerTimeVarying:
         distances = np.linalg.norm(config_goal - self.vertices[:self.vert_cnt, :-1], axis=1)
         mask_vertices_goal = distances < self.goal_region_radius
         if mask_vertices_goal.any():
-            print("Have goal vertices")
             times = self.vertices[:self.vert_cnt:, -1][mask_vertices_goal]
             i_min_time_sub = np.argmin(times)
             min_time = np.min(times)
@@ -177,13 +182,12 @@ class RRTPlannerTimeVarying:
             path.reverse()
             path = np.vstack(path)
         else:
-            print("No goal vertices..")
             path = np.array([]).reshape((0, state_start.size))
         return path
 
-    def sample_collision_free_config(self):
+    def sample_collision_free_config(self, time_horizon):
         while True:
-            t = np.random.randint(1, self.time_horizon)
+            t = np.random.randint(1, time_horizon)
             # TODO: should constrain sampling based on t... actuation etc.
             config = np.random.uniform(self.configuration_limits[:, 0], self.configuration_limits[:, 1])
             state = np.append(config, t)
@@ -229,11 +233,11 @@ class RRTPlannerTimeVarying:
 
 class ModifiedRRTPlannerTimeVarying(RRTPlannerTimeVarying):
 
-    def init_graph_with_all_start_config(self, state_start):
+    def init_graph_with_all_start_config(self, state_start, time_horizon):
         t_start = int(state_start[-1])
         state_src = state_start.copy()
         i_parent = self.vert_cnt - 1
-        for t in range(t_start + 1, self.time_horizon):
+        for t in range(t_start + 1, time_horizon):
             i_child = self.vert_cnt
             state_dst = state_src.copy()
             state_dst[-1] = t
@@ -243,9 +247,55 @@ class ModifiedRRTPlannerTimeVarying(RRTPlannerTimeVarying):
             else:
                 break
 
-    def plan(self, state_start, config_goal, max_planning_time=np.inf, min_planning_time=0):
+    def test_naive_solution(self, state_start, config_goal, nr_coll_steps, goal_region_radius):
+        state_prev = state_start
+        path = []
+        while True:
+            config_prev = state_prev[:-1]
+            t_prev = state_prev[-1]
+            config_delta = np.clip(config_goal - config_prev, -self.max_actuation, self.max_actuation)
+            config_nxt = config_prev + config_delta
+            state_nxt = np.append(config_nxt, t_prev + 1)
+            collision_free_transition = self.world.is_collision_free_transition(
+                state_src=state_prev,
+                state_dst=state_nxt,
+                nr_coll_steps=nr_coll_steps
+            )
+            is_in_global_goal = is_vertex_in_goal_region(
+                config_nxt,
+                config_goal,
+                goal_region_radius
+            )
+            if collision_free_transition:
+                state_prev = state_nxt
+                path.append(state_nxt)
+            else:
+                path.clear()
+                break
+            if is_in_global_goal:
+                break
+        path = np.vstack(path) if path else np.array([]).reshape((0,) + state_start.shape)
+        return path
+
+    def plan(
+            self,
+            state_start,
+            config_goal,
+            max_planning_time=np.inf,
+            min_planning_time=0,
+            time_horizon=300
+    ):
         self.add_vertex_to_tree(state_start)
-        self.init_graph_with_all_start_config(state_start)
+        self.init_graph_with_all_start_config(state_start, time_horizon)
+        naive_path = self.test_naive_solution(
+            state_start,
+            config_goal,
+            nr_coll_steps=self.local_planner.nr_coll_steps,
+            goal_region_radius=self.goal_region_radius
+        )
+        if naive_path.size > 0:
+            logger.debug("Naive path solves problem. Done!")
+            return naive_path, None
         time_s, time_elapsed = self.start_timer()
         found_solution = False
         while (
@@ -253,7 +303,7 @@ class ModifiedRRTPlannerTimeVarying(RRTPlannerTimeVarying):
                 time_elapsed < max_planning_time and
                 not found_solution
         ) or time_elapsed < min_planning_time:
-            state_free = self.sample_collision_free_config()
+            state_free = self.sample_collision_free_config(time_horizon)
             i_nearest, state_nearest = self.find_nearest_vertex(state_free)
             if i_nearest is None:
                 continue
@@ -266,11 +316,13 @@ class ModifiedRRTPlannerTimeVarying(RRTPlannerTimeVarying):
                 if is_vertex_in_goal_region(config_new, config_goal, self.goal_region_radius):
                     found_solution = True
                     if time_elapsed < min_planning_time:
-                        self.time_horizon = state_new[-1]
-                        print(
-                            f"Found solution with time horizon {self.time_horizon}."
-                            f" Min planning time left {min_planning_time - time_elapsed}"
+                        time_horizon = state_new[-1]
+                        logger.debug(
+                            "Found solution with time horizon %s. Min planning time left %s",
+                            time_horizon, min_planning_time - time_elapsed
                         )
+                    else:
+                        logger.debug("Found solution with time horizon %s.", time_horizon)
                     break
                 i_parent = i_child
             time_elapsed = time.time() - time_s
