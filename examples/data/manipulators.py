63b49655b1eeb2ac453aedf96a83851fb6806716
berwul <bernhard.f.k.wullt@gmail.com>
2023-01-05 18:49:46 +0100

    Add stuff

--- examples/data/manipulators.py
+++ examples/data/manipulators.py
@@ -27,7 +27,7 @@ DATA_MANIPULATOR_3DOF = {
         {
             "position": np.array([0.0, 0.0, 0.0]),
             "rotation": np.array([0.0, 0.0, 1.0]),
-            "limits": [-np.pi / 2, np.pi / 2]
+            "limits": [-np.pi * (3/4), np.pi * (3/4)]
         },
         {
             "position": np.array([0.0, 0.0, 0.0]),
