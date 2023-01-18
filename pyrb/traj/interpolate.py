a43f3b6f1bfa29e09b4290cc9989fb76435de43c
berwul <bernhard.f.k.wullt@gmail.com>
2023-01-18 14:37:46 +0100

    Add stuff

--- pyrb/traj/interpolate.py
+++ pyrb/traj/interpolate.py
@@ -3,11 +3,12 @@ import numpy as np
 
 def interpolate_path(sparse_path, segment_interpolator):
     path = []
-    for config_src, config_dst in zip(sparse_path[:-1], sparse_path[1:]):
-        segment = segment_interpolator(config_src, config_dst)
+    for src, dst in zip(sparse_path[:-1], sparse_path[1:]):
+        segment = segment_interpolator(src, dst)
         segment_no_dst = segment[:-1, :]
         path.append(segment_no_dst)
     return np.vstack(path + [sparse_path[-1]])
+
 
 
 def interpolate_along_line_l_infty_dim_2(config_src, config_dst, max_distance_per_step_per_dim):
@@ -28,7 +29,8 @@ def compute_max_distance_l_infty_dim_2(config_src, config_dst, dim_max_distance)
 def interpolate_along_line(config_src, config_dst, max_distance_per_step):
     distance = np.linalg.norm(config_dst - config_src)
     betas = np.arange(0, distance, max_distance_per_step) / distance
-    betas = np.append(betas, 1)
+    if not np.isclose(betas[-1], 1):
+        betas = np.append(betas, 1)
     betas = betas.reshape(-1, 1)
     nr_steps = betas.shape[0]
     config_src_many = np.tile(config_src.reshape(1, -1), (nr_steps, 1))
