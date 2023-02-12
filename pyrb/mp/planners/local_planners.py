26edef656f397b9f7782299376ed98883d672ea3
berwul <bernhard.f.k.wullt@gmail.com>
2023-02-12 14:54:23 +0100

    apa

--- pyrb/mp/planners/local_planners.py
+++ pyrb/mp/planners/local_planners.py
@@ -158,70 +158,122 @@ class LocalPlannerSpaceTime(BaseLocalPlanner):
         path.append(path_sparse[-1])
         return np.vstack(path)
 
-    # def bang_bang_control_to_destination(self, space, state_src, state_dst, max_distance=None, goal_region=None):
-    #     t_src, t_dst = state_src[-1], state_dst[-1]
-    #     assert t_src < t_dst
-    #     if max_distance is None:
-    #         max_distance, max_time_horizon = self.max_distance
-    #         t_end = self.transition(t_src, dt=max_time_horizon, time_horizon=min(space.max_time, t_dst))
-    #     else:
-    #         t_end = t_dst
-    #     waypoints = [state_src]
-    #     state_prev = state_src
-    #     config_dst = state_dst[:-1]
-    #     acc_distance = 0
-    #     config_delta_prev = None
-    #     max_actuation = self.max_actuation
-    #     while True:
-    #         config_prev, t_prev = state_prev[:-1], state_prev[-1]
-    #         if acc_distance < max_distance:
-    #             config_delta = np.clip(config_dst - config_prev, -max_actuation, max_actuation)
-    #             config_nxt = config_prev + config_delta
-    #             acc_distance += np.linalg.norm(config_delta)
-    #         else:
-    #             config_nxt = config_prev
-    #             config_delta = config_delta_prev
-    #
-    #         t_nxt = self.transition(t_prev, time_horizon=space.max_time)
-    #         state_nxt = np.append(config_nxt, t_nxt)
-    #         is_in_global_goal = goal_region is not None and goal_region.is_within(state_nxt)
-    #         collision_free_transition = space.is_collision_free_transition(
-    #             state_src=state_prev,
-    #             state_dst=state_nxt,
-    #             min_step_size=self.min_coll_step_size
-    #         )
-    #         end = not collision_free_transition or (t_nxt == t_end) or is_in_global_goal
-    #         changed_traj = config_delta_prev is not None and not np.isclose(config_delta_prev, config_delta).all()
-    #         if changed_traj or end:
-    #             if end:
-    #                 waypoints.append(state_nxt)
-    #             else:
-    #                 waypoints.append(state_prev)
-    #         state_prev = state_nxt
-    #         config_delta_prev = config_delta
-    #         if end:
-    #             break
-    #     return collision_free_transition, np.vstack(waypoints)
-    #
-    # def interpolate_path_from_waypoints(self, path_sparse):
-    #     path = []
-    #     max_actuation = self.max_actuation
-    #     for state_src, state_dst in zip(path_sparse[:-1], path_sparse[1:]):
-    #         t = int(state_dst[-1] - state_src[-1])
-    #         if t == 1:
-    #             path.append(state_src.copy())
-    #         else:
-    #             state = state_src
-    #             t_dst = state_dst[-1]
-    #             t_src = state[-1]
-    #             cnt = 0
-    #             while state[-1] != t_dst:
-    #                 path.append(state.copy())
-    #                 config_nxt = state[:-1] + np.clip(state_dst[:-1] - state[:-1], -max_actuation, max_actuation)
-    #                 cnt += 1
-    #                 state = np.append(config_nxt, t_src + cnt)
-    #     path.append(path_sparse[-1])
-    #     return np.vstack(path)
+    def transition(self, t, dt=1, time_horizon=np.inf):
+        return min(t + dt, time_horizon)
+
+
+class LocalPlannerSpaceTimeBangBang(BaseLocalPlanner):
+
+    def __init__(self, nr_coll_steps_between_transition, max_actuation, max_distance):
+        self.nr_coll_steps_between_transition = nr_coll_steps_between_transition
+        self.max_distance = max_distance
+        self.max_actuation = max_actuation
+
+    def plan(self, space, state_src, state_dst, max_distance=None, goal_region=None):
+        # assumes state_src is collision free
+        status, waypoints = self.generate_waypoints(
+            space, state_src, state_dst, max_distance, goal_region
+        )
+        return status, waypoints[1:]
+
+    def generate_waypoints(self, space, state_src, state_dst, max_distance=None, goal_region=None):
+        t_src, t_dst = state_src[-1], state_dst[-1]
+        if t_src > t_dst:
+            # path planning backwards in time
+            collision_free, waypoints = self.bang_bang_control_to_destination(
+                space,
+                state_dst,
+                state_src,
+                max_distance,
+                goal_region
+            )
+            status = self.coll_check_status(collision_free, waypoints, state_src)
+            waypoints = waypoints[::-1]
+        else:
+            collision_free, waypoints = self.bang_bang_control_to_destination(
+                space,
+                state_src,
+                state_dst,
+                max_distance,
+                goal_region
+            )
+            status = self.coll_check_status(collision_free, waypoints, state_dst)
+        return status, waypoints
+
+    def calculate_next_configuration(self, config_src, config_dst):
+        direction = config_dst - config_src
+        distance = np.linalg.norm(direction)
+        if distance > 0:
+            beta = np.clip(self.max_actuation / distance, 0, 1)
+        else:
+            beta = 0
+        return config_src * (1 - beta) + config_dst * beta
+
+    def bang_bang_control_to_destination(self, space, state_src, state_dst, max_distance=None, goal_region=None):
+        nr_steps = self.nr_coll_steps_between_transition + 1
+        t_src, t_dst = state_src[-1], state_dst[-1]
+        assert t_src < t_dst
+        if max_distance is None:
+            max_distance, max_time_horizon = self.max_distance
+            t_end = self.transition(t_src, dt=max_time_horizon, time_horizon=min(space.max_time, t_dst))
+        else:
+            t_end = t_dst
+        waypoints = [state_src]
+        state_prev = state_src
+        config_dst = state_dst[:-1]
+        acc_distance = 0
+        config_delta_prev = None
+        max_actuation = self.max_actuation
+        while True:
+            config_prev, t_prev = state_prev[:-1], state_prev[-1]
+            if acc_distance < max_distance:
+                config_delta = np.clip(config_dst - config_prev, -max_actuation, max_actuation)
+                config_nxt = config_prev + config_delta
+                acc_distance += np.linalg.norm(config_delta)
+            else:
+                config_nxt = config_prev
+                config_delta = config_delta_prev
+
+            t_nxt = self.transition(t_prev, time_horizon=space.max_time)
+            state_nxt = np.append(config_nxt, t_nxt)
+            is_in_global_goal = goal_region is not None and goal_region.is_within(state_nxt)
+            collision_free_transition = space.is_collision_free_transition(
+                state_src=state_prev,
+                state_dst=state_nxt,
+                nr_steps=nr_steps
+            )
+            end = not collision_free_transition or (t_nxt == t_end) or is_in_global_goal
+            changed_traj = config_delta_prev is not None and not np.isclose(config_delta_prev, config_delta).all()
+            if changed_traj or end:
+                if end:
+                    waypoints.append(state_nxt)
+                else:
+                    waypoints.append(state_prev)
+            state_prev = state_nxt
+            config_delta_prev = config_delta
+            if end:
+                break
+        return collision_free_transition, np.vstack(waypoints)
+
+    def interpolate_path_from_waypoints(self, path_sparse):
+        path = []
+        max_actuation = self.max_actuation
+        for state_src, state_dst in zip(path_sparse[:-1], path_sparse[1:]):
+            t = int(state_dst[-1] - state_src[-1])
+            if t == 1:
+                path.append(state_src.copy())
+            else:
+                state = state_src
+                t_dst = state_dst[-1]
+                t_src = state[-1]
+                cnt = 0
+                while state[-1] != t_dst:
+                    path.append(state.copy())
+                    config_nxt = state[:-1] + np.clip(state_dst[:-1] - state[:-1], -max_actuation, max_actuation)
+                    cnt += 1
+                    state = np.append(config_nxt, t_src + cnt)
+        path.append(path_sparse[-1])
+        return np.vstack(path)
 
     def transition(self, t, dt=1, time_horizon=np.inf):
         return min(t + dt, time_horizon)
